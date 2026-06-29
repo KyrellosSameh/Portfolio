3bfc89db77ddc564030490f51bd2b249b7661cad
unknown <kerlis447509@feng.bu.edu.eg>
2026-06-29 18:11:34 +0300

    final

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 
-const asyncCssPlugin = () => ({
-  name: 'async-css',
+const preloadImagePlugin = () => ({
+  name: 'preload-image',
   transformIndexHtml(html: string) {
-    let newHtml = html.replace(
-      /<link rel="stylesheet" crossorigin href="(.*?\.css)">/g,
-      '<link rel="preload" href="$1" as="style"><link rel="stylesheet" crossorigin href="$1" media="print" onload="this.media=\'all\'"><noscript><link rel="stylesheet" crossorigin href="$1"></noscript>'
-    );
-    return newHtml.replace(
+    return html.replace(
       '</head>',
       '  <link rel="preload" href="/profile.webp" as="image" type="image/webp" fetchpriority="high">\n  </head>'
     );
@@ -20,7 +16,7 @@ const asyncCssPlugin = () => ({
 
 export default defineConfig({
   plugins: [
-    asyncCssPlugin(),react(), tailwindcss()],
+    preloadImagePlugin(), react(), tailwindcss()],
   build: {
     target: 'esnext',
     cssCodeSplit: false,
